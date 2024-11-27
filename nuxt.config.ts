64178b8f3d993a917e1885819310bfb73eaa9aa4
Duong Hoang <dunghoinhau1@gmail.com>
2024-11-27 11:30:19 +0700

    add module i18n, naiveui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 export default defineNuxtConfig({
   ssr: true,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', 'nuxtjs-naive-ui'],
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "last" }],
+  },
+  i18n: {
+    vueI18n: './i18n.config.ts' // if you are using custom path, default
+  },
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="text-red-500">
     Index Page
+    <div class="flex gap-4">
+      <button @click="setLocale('en')">en</button>
+      <button @click="setLocale('vi')">vi</button>
+      <p>{{ $t('welcome') }}</p>
+    </div>
+    <n-button>Default</n-button>
   </div>
 </template>
 
 <script setup lang="ts">
+const { setLocale } = useI18n()
 
 </script>
 
