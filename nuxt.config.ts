d805b96e032fad3a66422095667155ce080c4848
Duong Hoang <dunghoinhau1@gmail.com>
2024-11-27 11:34:16 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   ssr: true,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', 'nuxtjs-naive-ui'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "last" }],
   },
